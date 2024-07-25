de4e49ff4cc4239394f4795f4d855ccb3e792dd7
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-07-25 18:01:07 +0530

    commit

---
+++ pattern1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    for (int i = 0; i < 3; i++)
+    {
+       for (int j = 0; j < 5; j++)
+       {
+        cout <<"*";
+       }
+       cout << endl;
+       
+    }
+    
+
+
+}
