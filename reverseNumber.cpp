46e62240e32041edc7de58eaac438f46134a7084
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-07-26 14:53:09 +0530

    commit

---
+++ reverseNumber.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int reverseNumber(int n){
+   
+}
+
+int main(){
+    int n;
+    cout<<"enter a number:";
+    
+    cin>>n;
+
+    if (n<0)
+    {
+        cout << "the factorial of less than zero does not exist:";
+
+    }
+    else
+    {
+        cout << "the factorial of " << n << " is " << Factorial(n);
+    }
+    
+    
+    return 0;
+}
+
