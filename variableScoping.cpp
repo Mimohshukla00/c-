272c2ac7f68540529a5dfab22f55b92f0ac0191b
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-07-26 14:53:31 +0530

    commit

---
+++ variableScoping.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;  // declare int 
+    a=10 ;
+
+    int b=10; // intialaise
+
+    b=10; ///update of variable 
+
+    // int b=10;//redefined 
+
+    cout << "a value: "<< a << "b value :"<< b;
+
+   if (b==10)
+   {
+    int a =20;
+    cout <<"value of scoped a;"<<a;
+    
+   }
+   
+    
+
+
+}
