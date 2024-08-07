1214aeaf99ff4b502942836122cd6eee7be48202
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-08-07 22:32:33 +0530

    commit

--- leetcodee/unbounded.cpp
+++ leetcodee/unbounded.cpp
@@ -1,30 +1,13 @@
 #include<iostream>
 using namespace std;
-
 // this is the method for binary search of sorted and infinite type array
 // also called unbounded search 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int binary_search(int arr[],int start, int end,int key){
     while (start<=end)
     {
         int mid=start+(end-start)/2;
         if (arr[mid]==key){
             return mid;
-
         }
         else if (arr[mid]<key)
         {
@@ -32,14 +15,11 @@ int binary_search(int arr[],int start, int end,int key){
         }
         else{
             end=mid-1;
-        }
-        
+        }  
     }
     return -1;
     
 }
-
-
 int findIndex(int arr[],int key){
     int start = 0;
     int end=1;
@@ -48,9 +28,7 @@ int findIndex(int arr[],int key){
         start=end;
         end=2*end;
     }
-
     return binary_search(arr,start,end ,key);
-    
 }
 int main(){
 
@@ -58,6 +36,5 @@ int main(){
     int key=500;
     int ans=findIndex(arr,key);
     cout<<ans<<endl;
-
     return 0;
 }
